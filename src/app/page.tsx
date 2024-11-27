2fe7d7d51fc58c690ddcc6ed5095a55e3c8bb656
Raziel Navarro <elrazand@gmail.com>
2024-11-27 02:17:57 -0500

    Added Navbar to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <>
     <header>
-
+      <Navbar />
     </header>
     <main></main>
     </>
