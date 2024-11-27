e742b26396ce9dd7738fcafb723fbdb1e7737e44
Raziel Navarro <elrazand@gmail.com>
2024-11-27 02:17:22 -0500

    Set up initial layout of Navbar

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,23 @@
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-600 text-white py-4">
+        <div className="container mx-auto flex items-center justify-between">
+        {/* Title on navbar */}
+        <div className="text-2xl">
+            <span className="text-purple-400">Raziel</span>
+        </div>
+
+        {/* Navigation links */}
+
+      <ul>
+        <li>
+        </li>
+        <li>
+        </li>
+      </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
