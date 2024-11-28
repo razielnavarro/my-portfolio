9056df8d52581c1b1c241b7fe89ae51696ad998e
Raziel Navarro <elrazand@gmail.com>
2024-11-28 03:00:14 -0500

    Added navigation links titles and GitHub icon to Navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -4,17 +4,27 @@ const Navbar = () => {
         <div className="container mx-auto flex items-center justify-between">
         {/* Title on navbar */}
         <div className="text-2xl">
-            <span className="text-purple-400">Raziel</span>
+            <span className="text-white">Raziel</span>
         </div>
 
         {/* Navigation links */}
-
-      <ul>
+      <ul className="flex flex-col md:flex-row md:space-x-8 mt-4 md:mt-0 text-sm font-medium">
         <li>
+          Home
         </li>
         <li>
+          About
+        </li>
+        <li>
+          Projects
         </li>
       </ul>
+
+      {/* GitHub icon */}
+      <a href="#">
+        <img className="w-7" src="assets\github-mark-white.svg"></img>
+      </a>
+
       </div>
     </nav>
   );
