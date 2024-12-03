a6098b7c1edf708cb0ca5de11efd82771915c9bc
Raziel Navarro <elrazand@gmail.com>
2024-12-03 01:43:16 -0500

    Section with skills added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,8 +23,21 @@ export default function Home() {
       <aside className="flex justify-center md:justify-start">
         <img alt="Kirby" src="assets\Kirby.gif" className="w-10/12 md:h-auto"></img>
       </aside>
-
     </main>
+
+      {/* Section with skills */}
+      <h1 className="mx-auto text-center mt-14 mb-1 text-2xl">Skills</h1>
+        <section className="grid grid-cols-2 md:grid-cols-4 gap-4 md:gap-2 justify-items-center w-auto md:w-3/4 lg:w-2/4 mx-auto">
+        <div className="text-center text-xs md:text-md"><img className="w-32 h-32 md:w-60 md:h-60 object-contain" src="assets/html.png" alt="html"></img>HTML</div>
+        <div className="text-center text-xs md:text-md"><img className="w-32 h-32 md:w-60 md:h-60 object-contain" src="assets/css.png" alt="css"></img>CSS</div>
+        <div className="text-center text-xs md:text-md"><img className="w-32 h-32 md:w-60 md:h-60 object-contain" src="assets/js.png" alt="javascript"></img>JavaScript</div>
+        <div className="text-center text-xs md:text-md"><img className="w-32 h-32 md:w-60 md:h-60 object-contain" src="assets/ts.png" alt="typescript"></img>TypeScript</div>
+        <div className="text-center text-xs md:text-md"><img className="w-32 h-32 md:w-60 md:h-60 object-contain" src="assets/react.png" alt="react"></img>React</div>
+        <div className="text-center text-xs md:text-md"><img className="w-32 h-32 md:w-60 md:h-60 object-contain" src="assets/next.png" alt="next.js"></img>Next.js</div>
+        <div className="text-center text-xs md:text-md"><img className="w-32 h-32 md:w-60 md:h-60 object-contain" src="assets/tailwind.png" alt="tailwind css"></img>Tailwind CSS</div>
+        <div className="text-center text-xs md:text-md"><img className="w-32 h-32 md:w-60 md:h-60 object-contain" src="assets/node.png" alt="node.js"></img>Node.js</div>
+      </section>
+
     </>
   );
 }
