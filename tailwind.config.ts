d85261e04bd9577b53bcfbb10a8a96753df1ea05
Raziel Navarro <elrazand@gmail.com>
2024-12-19 14:38:40 -0500

    chore: add infinite scroll animation and keyframes to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,15 @@ export default {
       backgroundImage: {
         'purple-gradient': 'linear-gradient(90deg, rgba(241,245,249,1) 0%, rgba(191,161,247,1) 35%, rgba(116,67,208,1) 100%)',
       },
+      keyframes: {
+        'infinite-scroll' : {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(calc(-50% - 20px))' },
+        },
+    },
+      animation: {
+      'infinite-scroll': 'infinite-scroll 15s linear infinite',
+    },
     },
   },
   plugins: [],
