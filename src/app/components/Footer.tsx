42e7fb5fc596d0fc2dd866998e9a373bd9ee7f97
razielnavarro <elrazand@gmail.com>
2025-04-30 15:38:27 -0500

    fix: update LinkedIn profile link in footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
         <div className="container flex justify-between mx-auto px-1 text-xs md:text-sm">
             <p>© 2024 Raziel Navarro</p>
             <div className="flex space-x-4">
-            <a href="https://www.linkedin.com/in/raziel-navarro-3a2b8b1b2/" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.linkedin.com/in/razielnavarro/" target="_blank" rel="noopener noreferrer">
                 <img className="w-5 h-5 md:w-7 md:h-7" src="assets/linkedin.png" alt="linkedin"></img>
                 </a>
                 <a href="https://github.com/razielnavarro" target="_blank" rel="noopener noreferrer">
